6e18fb4021c9da2129f88437acb48755da41a73b
iCharss <rdgz.carlos12@gmail.com>
2025-07-03 23:38:44 -0300

    Rep-Pan

--- app/email_sender.php
+++ app/email_sender.php
@@ -16,8 +16,8 @@ function sendEmails($compra, $cart) {
         $mail->isSMTP();
         $mail->Host = 'smtp.hostinger.com'; // Servidor SMTP de Hostinger
         $mail->SMTPAuth = true;
-        $mail->Username = '';
-        $mail->Password = ''; 
+        $mail->Username = ' ';
+        $mail->Password = ' '; 
         $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS; // SSL
         $mail->Port = 465; 
 
